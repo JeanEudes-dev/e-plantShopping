8019e592b967dd02d7cf68b6b1d9afdcbdbba10c
JeanEudes-dev <jeudesmails@gmail.com>
2024-10-21 20:49:49 +0300

    modify ProductList CartSlice and cartitem components

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -1,19 +1,32 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 export const CartSlice = createSlice({
-  name: 'cart',
+  name: "cart",
   initialState: {
     items: [], // Initialize items as an empty array
   },
   reducers: {
     addItem: (state, action) => {
-    
+      const itemExists = state.items.find(
+        (item) => item.name === action.payload.name
+      );
+      if (itemExists) {
+        itemExists.quantity += 1;
+      } else {
+        state.items.push({ ...action.payload, quantity: 1 });
+      }
     },
     removeItem: (state, action) => {
+      state.items = state.items.filter(
+        (item) => item.name !== action.payload.name
+      );
     },
     updateQuantity: (state, action) => {
-
-    
+      const { name, quantity } = action.payload;
+      const item = state.items.find((item) => item.name === name);
+      if (item) {
+        item.quantity = quantity;
+      }
     },
   },
 });
